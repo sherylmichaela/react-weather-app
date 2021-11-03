0e73a23edc4c1fc0405139ad6d5a636a0da158e0
sherylmichaela <89667702+sherylmichaela@users.noreply.github.com>
2021-11-03 22:39:27 +1100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sheryl Chee and is{" "}
+        <a
+          href="https://github.com/sherylmichaela/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
