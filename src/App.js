257f0302cf13b85c4b71f702528139d302ba4cb3
sherylmichaela <89667702+sherylmichaela@users.noreply.github.com>
2021-11-09 23:55:03 +1100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sheryl Chee and is{" "}
-        <a
-          href="https://github.com/sherylmichaela/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Sheryl Chee and is{" "}
+          <a
+            href="https://github.com/sherylmichaela/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
